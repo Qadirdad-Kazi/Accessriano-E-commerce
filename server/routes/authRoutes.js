819ed3f5300aaf5678137567c51ae73a40236301
Qadirdad-Kazi <qadirdadkazi@gmail.com>
2025-02-13 00:14:11 +0500

    vercel api dep

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middleware/auth');
 
-// Registration route
+// ✅ Registration route
 router.post('/register', authController.registerUser);
 
-// Login route
+// ✅ Login route
 router.post('/login', authController.loginUser);
 
-// Get user profile (Protected)
-const authMiddleware = require('../middleware/auth');
+// ✅ Get user profile (protected)
 router.get('/profile', authMiddleware, authController.getProfile);
 
 module.exports = router;
