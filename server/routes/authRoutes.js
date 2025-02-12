1cec7765089369d434b058bcae78ec1d132349b0
Qadirdad-Kazi <qadirdadkazi@gmail.com>
2025-02-12 20:53:02 +0500

    vercel config

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const auth = require('../middleware/auth');
 
+// Registration route
 router.post('/register', authController.registerUser);
+
+// Login route
 router.post('/login', authController.loginUser);
-router.get('/profile', auth, authController.getProfile);
+
+// Get user profile (Protected)
+const authMiddleware = require('../middleware/auth');
+router.get('/profile', authMiddleware, authController.getProfile);
 
 module.exports = router;
